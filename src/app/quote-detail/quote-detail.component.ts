cfe91ae2d40f74ddf6d2dc8b0f142d35288d510c
Kisavi <deniskisavi@gmail.com>
2022-04-12 00:54:23 +0300

    set function to call api on button click

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+//import hhtpclient
+import { HttpClient } from '@angular/common/http';
+// import Quote class
+import { Quote } from '../quote-class/quote';
 
 @Component({
   selector: 'app-quote-detail',
@@ -6,10 +10,23 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./quote-detail.component.css']
 })
 export class QuoteDetailComponent implements OnInit {
+  // create a property quote and assign it type Quote
+  quote!: Quote;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  showQuote() {
+    interface ApiResponse {
+      author: string;
+      quote: string;
+    }
+
+    this.http.get<ApiResponse>("http://quotes.stormconsultancy.co.uk/random.json").subscribe(data => {
+      // only executed when the request to the API is succesful.
+      this.quote = new Quote(data.author, data.quote)
+    })
   }
 
 }
